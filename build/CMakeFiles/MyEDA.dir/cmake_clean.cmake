6687bc205fd937df5981c519458b21ee785bdb2c
Amine ZNIYED <67275310+amine0007@users.noreply.github.com>
2025-12-12 21:33:08 +0000

    version 0

---
+++ build/CMakeFiles/MyEDA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyEDA.dir/src/main.cpp.o"
+  "CMakeFiles/MyEDA.dir/src/main.cpp.o.d"
+  "MyEDA"
+  "MyEDA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyEDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
